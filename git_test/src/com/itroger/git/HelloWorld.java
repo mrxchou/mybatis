f9b17c86744eb699d408e335ec7592ccb0595109
Roger <mrroger_xc@163.com>
2020-12-17 16:45:48 +0800

    idea commit java file second

--- git_test/src/com/itroger/git/HelloWorld.java
+++ git_test/src/com/itroger/git/HelloWorld.java
@@ -1,14 +1,8 @@
 package com.itroger.git;
 
-/**
- * @ClassName: HelloWorld
- * @Author Administrator
- * @Date 2020/12/17 16:37
- * @Description:
- * @Version 1.0
- */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+        System.out.println("Hello,World222!");
     }
 }
