929ff076a8853c0cfec7fe61c9c068c57c902923
Roger <68274033+mrxchou@users.noreply.github.com>
2021-01-12 20:40:09 +0800

    Delete HelloWorld.java

--- git_test/src/com/itroger/git/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.itroger.git;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello,World!");
-        System.out.println("Hello,World222!");
-        System.out.println("Hello,World333!");
-    }
-}
